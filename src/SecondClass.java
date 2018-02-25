adaeb8877f4b26454f018df9633172325efc3cb0
Adrian <Adrian@192.168.0.129>
2018-02-26 01:17:21 +0200

    First commit

---
+++ src/SecondClass.java
@@ -0,0 +1,23 @@
+
+public class SecondClass {
+	private int a;
+	
+	public SecondClass(){
+		a=5;
+	}
+	
+	public int getA(){
+		return a;
+	}
+	/**
+	 * @param args
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SecondClass A = new SecondClass();
+		System.out.println("Valoarea lui a este "+A.getA());
+	}
+
+}
